f92c4080dfb6146f8a9d03835d5416bf4fc9e1fb
Natanael Saymon <nsaymonsouza@gmail.com>
2020-03-20 18:01:48 -0300

    modal inserido

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,21 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 
-const cards = codument.querySelector('.card')
+const cards = document.querySelectorAll('.card')
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        const cardId = card.getAttribute('id') /*pega o atributo id*/
+        // const autor = card.querySelector('#receita-autor')
+        modalOverlay.classList.add('active') /*adicionando a classe active*/
+        modalOverlay.querySelector('img').src=`./assets/imagens/${cardId}.png`
+    })
+}
+
+
+/*function de fechar modal*/
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+    // modalOverlay.querySelector('iframe').src=""
+})
+
 
